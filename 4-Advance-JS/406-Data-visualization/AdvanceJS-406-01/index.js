b9519fa5c5c7cf27dbddfbc1c80c191d209cbcf8
username <sanodiyavijendra65@gmail.com>
2022-11-02 11:06:59 -0400

    update 406-done

---
+++ 4-Advance-JS/406-Data-visualization/AdvanceJS-406-01/index.js
@@ -0,0 +1,36 @@
+
+const showres=(e)=>{
+    display(e.target.value);
+    var h2=document.getElementById("h2")
+         h2.innerHTML=` ${e.key} and ${e.keyCode} ` ;
+         h2.style.opacity=1
+         h2.style.color="crimson"
+         h2.style.font ="40px"
+ }
+ function display(element){
+     // console.log(element);
+     const filter=countries_data.filter((value)=>{
+         if(value.name.startsWith(element)){
+             return true;
+         }
+         return false;
+     });
+ 
+     const parent = document.getElementById("table");
+     parent.innerHTML="";
+     filter.forEach((e)=>{
+         const new_row=document.createElement("tr");
+         const new_countries=document.createElement("td");
+         new_countries.innerHTML=e.name;
+         new_row.appendChild(new_countries);
+         parent.appendChild(new_row);
+         
+ 
+     });
+ }
+ 
+  
+   
+ 
+  
+ document.getElementById("search-form").addEventListener("keyup",showres);
